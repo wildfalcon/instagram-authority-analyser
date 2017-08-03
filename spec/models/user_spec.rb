c40f146c40c4ce08efc1f9d4b8b34dc85cdea44e
Laurie Young <laurie@wildfalcon.com>
2017-08-03 10:51:48 +0100

    Add #instagram_client to User

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,4 +2,25 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
 
+
+  describe "#instagram_client" do
+    let(:user) { User.new }
+
+    context "with identity" do
+      let(:identity) {stub_model(Identity, :access_token => "123abc")}
+      before { user.identity = identity}
+
+      it 'returns an instagram client' do
+        expect(user.instagram_client).to be_instance_of Instagram::Client
+      end
+    end
+
+    context "with missing identity" do
+      it 'should raise an exception' do
+        expect{user.instagram_client}
+          .to raise_exception Exceptions::InstagramError
+      end
+    end
+  end
+
 end
